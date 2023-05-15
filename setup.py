6677c9c0ea37bc7d6fb5ea558352f187bae3e492
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-05-15 16:26:56 +0800

    Init REPO

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open("./requirements.txt", "r") as f:
+    requirements = [l.strip() for l in f.readlines() if len(l.strip()) > 0]
+
+setup(
+    name="layout_models",
+    version="0.1",
+    packages=find_packages(),
+    install_requires=requirements,
+    author="Enrique Solarte",
+    author_email="enrique.solarte.pardo@gmail.com",
+    description=("Layout estimation models (wrappers)."),
+    license="BSD",
+)
