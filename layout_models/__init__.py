4188241be04d6f523eb0244696ae53068bad9800
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2023-09-03 11:26:04 +0800

    Add VERSION to the REPO
    modified:   layout_models/__init__.py
    * Access by `from layout_models import VERSION`

--- layout_models/__init__.py
+++ layout_models/__init__.py
@@ -1,7 +1,9 @@
-
 from layout_models.utils import load_layout_model
+from geometry_perception_utils.config_utils import get_repo_version
 import os
 
 LY_MODELS_ROOT = os.path.dirname(os.path.abspath(__file__))
 LY_MODELS_CFG = os.path.join(LY_MODELS_ROOT, 'config')
 os.environ['LY_MODELS_CFG'] = LY_MODELS_CFG
+
+VERSION = get_repo_version(DIR_GEOM_UTILS)
