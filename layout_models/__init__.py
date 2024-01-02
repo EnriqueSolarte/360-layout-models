d9af54c8ba07be20e65213374162549b19526a1c
EnriqueSolarte <enrique.solarte.pardo@gmail.com>
2024-01-02 08:57:16 +0800

    Add function-oriented implementation of WrapperHorizonNet

--- layout_models/__init__.py
+++ layout_models/__init__.py
@@ -4,6 +4,8 @@ import os
 
 LY_MODELS_ROOT = os.path.dirname(os.path.abspath(__file__))
 LY_MODELS_CFG = os.path.join(LY_MODELS_ROOT, 'config')
+LY_MODELS_ASSETS = os.path.join(LY_MODELS_ROOT, 'assets')
 os.environ['LY_MODELS_CFG'] = LY_MODELS_CFG
+os.environ['LY_MODELS_ASSETS'] = LY_MODELS_ASSETS
 
 VERSION = get_repo_version(__file__)
--- layout_models/utils.py
+++ layout_models/utils.py
@@ -1,12 +1,16 @@
-from layout_models.horizon_net_wrapper.wrapper_horizon_net import WrapperHorizonNet
 
 def load_layout_model(cfg):
     """
     Load a layout model estimator and returns an instance of it
     """
-    if cfg.model.ly_model == "HorizonNet":
+
+    if cfg.ly_model == "HorizonNet":
+        from layout_models.horizon_net_wrapper.wrapper_horizon_net import WrapperHorizonNet
         # ! loading HorizonNet
         model = WrapperHorizonNet(cfg)
+    if cfg.ly_model == "HorizonNetV2":
+        from layout_models.horizon_net_wrapper.wrapper_horizon_net_new import WrapperHorizonNet as WrapperHorizonNetV2
+        model = WrapperHorizonNetV2(cfg)
     else:
         raise NotImplementedError("")
 
